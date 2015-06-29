235c1835ea1b04d8ad281dd7ef2382074fe2716c
CanRau <cansrau@gmail.com>
2015-06-29 18:31:22 -0500

    changed file name

--- pushStateAccordion.js
+++ pushStateAccordion.js
@@ -1,5 +1,5 @@
 /*
- * simple pushStateAccordion | https://github.com/CanRau/pushStateAccordion | Licensed under GNU/GPL v3 http://www.gnu.org/licenses/gpl-3.0 
+ * simple pushStateAccordion | http://www.happygaia.com/ | Published under GPL http://www.gnu.org/licenses/gpl-3.0 
  */
 $(window).on("popstate", function(e) {
     if( window.location.href.indexOf("?") >= 0 )
@@ -12,8 +12,20 @@ $(window).on("popstate", function(e) {
 $("[data-tools=accordion]").click(function(e) {
     e.preventDefault();
     var toBeToggled = $(this).attr("href").split('?')[1];
-    $("[data-accordion="+toBeToggled+"]").slideToggle('normal');
-    $("[data-accordion]").not($("[data-accordion="+toBeToggled+"]")).slideUp('slow');
+    // $("[data-accordion="+toBeToggled+"]").slideToggle('normal');
+    // $("[data-accordion]").not($("[data-accordion="+toBeToggled+"]")).slideUp('slow');
+
+
+    if($(this).attr("data-accordion-style") == 'tab')
+    {
+        $("[data-accordion="+toBeToggled+"]").fadeToggle("normal");
+        $("[data-accordion]").not($("[data-accordion="+toBeToggled+"]")).fadeOut('normal');
+    }
+    else
+    {
+        $("[data-accordion="+toBeToggled+"]").slideToggle('normal');
+        $("[data-accordion]").not($("[data-accordion="+toBeToggled+"]")).slideUp('normal');
+    }
 
     // change url of address bar
     var url = window.location.href;
@@ -23,7 +35,7 @@ $("[data-tools=accordion]").click(function(e) {
     else window.history.pushState('', '', href);
     return false;
 });
-$("[data-accordion-close]").on('click', function(e){
+$(".btn-cancel").on('click', function(e){
     $("[data-accordion]").slideUp("slow");
     window.history.pushState('', '', window.location.href.split("?")[0]);
 });
